6584113d0a99283a13bb0d283a8f9279c0b10f8f
Jason Yoon <jasony0206@gmail.com>
2016-05-23 13:21:00 -0700

    Initialize project

---
+++ PeriodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeriodTracker
+//
+//  Created by Jason Yoon on 5/23/16.
+//  Copyright © 2016 Jason Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
