bf9a341c04747e5f3f87759ccee595e0d44cefcc
Jason Yoon <jasony0206@gmail.com>
2016-05-26 10:52:19 -0700

    Add delete feature

--- PeriodTracker/PeriodTableViewController.swift
+++ PeriodTracker/PeriodTableViewController.swift
@@ -15,6 +15,9 @@ class PeriodTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.rightBarButtonItem = editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,26 +53,22 @@ class PeriodTableViewController: UITableViewController {
         return strDate
     }
     
-    /*
-    // Override to support conditional editing of the table view.
+    // Support delete feature
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            dates.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
