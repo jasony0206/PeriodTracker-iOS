e555f8a1890a213b685b82a32d1fada9b4ba48b9
Jason Yoon <jasony0206@gmail.com>
2016-05-29 20:21:25 -0700

    New profile view 3 dates working properly

--- PeriodTracker/ProfileTableViewController.swift
+++ PeriodTracker/ProfileTableViewController.swift
@@ -1,15 +1,15 @@
 //
-//  ViewController.swift
+//  ProfileTableViewController.swift
 //  PeriodTracker
 //
-//  Created by Jason Yoon on 5/23/16.
+//  Created by Jason Yoon on 5/29/16.
 //  Copyright © 2016 Jason Yoon. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController, ChangeMemberDelegate {
-    
+class ProfileTableViewController: UITableViewController, ChangeMemberDelegate {
+
     // MARK: Properties
     var dates = [NSDate]()
     var avgCycle = 28
@@ -36,24 +36,24 @@ class ProfileViewController: UIViewController, ChangeMemberDelegate {
             nextPeriodLabel.text = "?"
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     @IBAction func unwindToProfile(sender: UIStoryboardSegue) {
         if let sourceViewController = sender.sourceViewController as? AddPeriodViewController,
-        newDate: NSDate = sourceViewController.pickedDate as NSDate {
-            dates.append(newDate)
-            updateProfile()
+            newDate: NSDate = sourceViewController.pickedDate as NSDate {
+                dates.append(newDate)
+                updateProfile()
         }
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "DateListSegue") {
             let detailVC = segue.destinationViewController as! PeriodTableViewController;
-            detailVC.delegate = self
-            detailVC.dates = self.dates
+//            detailVC.delegate = self
+//            detailVC.dates = self.dates
         }
     }
     
@@ -118,20 +118,19 @@ class ProfileViewController: UIViewController, ChangeMemberDelegate {
     }
 }
 
-//extension NSDate {
-//    func numberOfDaysUntilDateTime(toDateTime: NSDate, inTimeZone timeZone: NSTimeZone? = nil) -> Int {
-//        let calendar = NSCalendar.currentCalendar()
-//        if let timeZone = timeZone {
-//            calendar.timeZone = timeZone
-//        }
-//        
-//        var fromDate: NSDate?, toDate: NSDate?
-//        
-//        calendar.rangeOfUnit(.Day, startDate: &fromDate, interval: nil, forDate: self)
-//        calendar.rangeOfUnit(.Day, startDate: &toDate, interval: nil, forDate: toDateTime)
-//        
-//        let difference = calendar.components(.Day, fromDate: fromDate!, toDate: toDate!, options: [])
-//        return difference.day
-//    }
-//}
-
+extension NSDate {
+    func numberOfDaysUntilDateTime(toDateTime: NSDate, inTimeZone timeZone: NSTimeZone? = nil) -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        if let timeZone = timeZone {
+            calendar.timeZone = timeZone
+        }
+        
+        var fromDate: NSDate?, toDate: NSDate?
+        
+        calendar.rangeOfUnit(.Day, startDate: &fromDate, interval: nil, forDate: self)
+        calendar.rangeOfUnit(.Day, startDate: &toDate, interval: nil, forDate: toDateTime)
+        
+        let difference = calendar.components(.Day, fromDate: fromDate!, toDate: toDate!, options: [])
+        return difference.day
+    }
+}
